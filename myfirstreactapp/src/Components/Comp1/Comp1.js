9521268c456515cd915706a81de4e0d567f24ac0
muthurajbharathi831 <muthurajbharathi@gmail.com>
2021-06-08 21:37:34 +0530

    my first react app

---
+++ myfirstreactapp/src/Components/Comp1/Comp1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Comp1.css'
+export default function Comp1() {
+    return (
+        <div className= "comp">
+            <h1>Learning React</h1>
+            <p>This is user defined Functional Component-one</p>
+
+            <a href="https://reactjs.org/">Read more</a>
+        </div>
+    )
+}
